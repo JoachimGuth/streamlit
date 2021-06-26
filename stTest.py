f20135d9dc2a364e9865eec7f930d57e613ef8f0
Joachim <77875845+JoachimGuth@users.noreply.github.com>
2021-06-26 22:19:04 +0800

    Update stTest.py

--- stTest.py
+++ stTest.py
@@ -31,7 +31,7 @@ st.sidebar.selectbox("Year:", year)
 
 # List of Month
 monthlist= {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
-month = [m for m in monthlis.keys()]
+month = [m for m in monthlist.keys()]
 st.write(month)
 
 #monthSelect = st.sidebar.selectbox ("Month: ", [ m for m in monthlist])
