51ef9cda18c2543e4a823f14d8efd0323ca500b8
Joachim <77875845+JoachimGuth@users.noreply.github.com>
2021-06-26 20:54:19 +0800

    Update stTest.py

--- stTest.py
+++ stTest.py
@@ -13,39 +13,3 @@ st.write(pulp.operating_system)
 
 
 
-# Plant Setup
-# Machines available for production
-machines = ['M1', 'M2', 'M3', 'M4', 'M5', 'M6', 'M7', 'M8']
-st.sidebar.write ("Machines in the plant"+ str(machines))
-
-# Standard Operating Hours per Day
-stddayAvailHours = 24
-st.sidebar.write("Available hours pere day: "+ str(stddayAvailHours))
-
-# Current Year used to create Production Calendar
-curYear = 2021
-st.sidebar.write("Available hours pere day: "+ str(curYear))
-
-# List of Month
-monthlist= {'Jan':1, 'Feb':2, 'Mar':3, 'Apr':4, 'May':5, 'Jun':6, 'Jul':7, 'Aug':8, 'Sep':9, 'Oct':10, 'Nov':11, 'Dec':12}
-print(monthlist['Jan'])
-
-# Nbr of Days of a month in a particulat year
-class DaysPerMonth(object):
-   def numberOfDays(self, y, m):
-      leap = 0
-      if y % 400 == 0:
-         leap = 1
-      elif y % 100 == 0:
-         leap = 0
-      elif y % 4 == 0:
-         leap = 1
-      if m==2:
-         return 28 + leap
-      list = [1,3,5,7,8,10,12]
-      if m in list:
-         return 31
-      return 30
-
-# Instance of the Class Days per Month
-dpm = DaysPerMonth()
