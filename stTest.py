4da61f9827086f87cf859e430b417b22f9fb257f
Joachim <77875845+JoachimGuth@users.noreply.github.com>
2021-07-12 12:48:00 +0800

    Update stTest.py

--- stTest.py
+++ stTest.py
@@ -112,7 +112,7 @@ def viewCalendar():
       st.subheader('Current Shift Settings')
       # Display current selection and shift configuration in the main window
       dfs = shiftCal[shiftCal['Month'].isin(['Jan']) & shiftCal['Machine'].isin(['M1'])]
-      st..plot(x="Day", y=['ShftHrs'], kind =  'bar')
+      st.plot(x="Day", y=['ShftHrs'], kind =  'bar')
       #plt.show()
       
       
